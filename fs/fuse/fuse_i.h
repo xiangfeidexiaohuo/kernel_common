24955a554f9266e1996d0839613ca9e52dea34c4
Daniel Rosenberg <drosen@google.com>
2025-02-04 17:14:23 -0800

    ANDROID: fuse: Adjust Passthrough for writeback cache
    This adjusts some tracking to allow passthrough and writeback cache to
    co-exist with eachother
    
    Change-Id: I5b8b3940a8c096cd3598d7c188439c6c636c01a1
    Bug: 372951405
    Fixes: 3ae410445fb1 ("ANDROID: fuse: Allow passthrough and not passthrough")
    test: atest android.scopedstorage.cts.general.ScopedStorageDeviceTest
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -136,6 +136,9 @@ struct fuse_inode {
 			/** Number of files/maps using page cache */
 			int iocachectr;
 
+			/* Number of files using passthrough */
+			int iopassctr;
+
 			/* Waitq for writepage completion */
 			wait_queue_head_t page_waitq;
 
@@ -261,7 +264,7 @@ struct fuse_file {
 	wait_queue_head_t poll_wait;
 
 	/** Does file hold a fi->iocachectr refcount? */
-	enum { IOM_NONE, IOM_CACHED, IOM_UNCACHED } iomode;
+	enum { IOM_NONE, IOM_CACHED, IOM_UNCACHED, IOM_PASSTHROUGH } iomode;
 
 #ifdef CONFIG_FUSE_PASSTHROUGH
 	/** Reference to backing file in passthrough mode */
